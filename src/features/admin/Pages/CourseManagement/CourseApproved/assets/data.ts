af3150c95985ea0c86181d6a0dcf500ee3686fd6
G-Mursalin <golammursalin309@gmail.com>
2024-12-04 00:53:17 +0600

    Add admin course approved UI

---
+++ src/features/admin/Pages/CourseManagement/CourseApproved/assets/data.ts
@@ -0,0 +1,192 @@
+export const data = {
+  _id: '6740999389d577bad488162b',
+  name: 'Advanced web development',
+  image: {
+    diskType: 'image/png',
+    path: 'https://f004.backblazeb2.com/file/all-users/courseCoverImage/6724fd912384ed2d3c261310-1732286861164-489926585-ba-courses.png',
+    originalName: 'BA-Courses.png',
+    modifiedName:
+      '6724fd912384ed2d3c261310-1732286861164-489926585-ba-courses.png',
+    fileId:
+      '4_z9b8c723f28a0f605912f081e_f112e61d8aeba7482_d20241122_m144744_c004_v0402027_t0010_u01732286864666',
+  },
+  details:
+    'Master modern web development with this comprehensive course. Learn React, Node.js, and MongoDB to build full-stack applications. Includes hands-on projects and real-world case studies.',
+  lessons: [
+    {
+      _id: '6740a6d0477c8e8ffd6ee8d6',
+      number: 'Lesson 1',
+      name: 'Introduction to Web Development',
+      recodedClasses: [
+        {
+          _id: '6740b9d43e34ecee7537353f',
+          recodeClassName: 'What is Web Devlopment ?',
+          classDate: '2025-01-05T14:30:00.000Z',
+          classDetails: 'In-depth exploration of web devlopmen',
+          classVideoURL: ['https://youtube.com/watch?v=react-hooks-lesson1'],
+        },
+      ],
+      resources: [
+        {
+          _id: '6740af6f710f1c7ead9a0f8a',
+          name: 'First Module Resources',
+          resourceDate: '2025-01-15T14:00:00.000Z',
+          uploadFileResources: [
+            {
+              diskType: 'application/pdf',
+              path: 'https://f004.backblazeb2.com/file/all-users/courseResources/6724fd912384ed2d3c261310-1732292457908-48188019-lorem-ipsum.pdf',
+              originalName: 'lorem-ipsum.pdf',
+              modifiedName:
+                '6724fd912384ed2d3c261310-1732292457908-48188019-lorem-ipsum.pdf',
+              fileId:
+                '4_z9b8c723f28a0f605912f081e_f113faed7bc6a1625_d20241122_m162102_c004_v0402027_t0003_u01732292461876',
+            },
+          ],
+        },
+      ],
+      assignments: [
+        {
+          _id: '6740b18b698af90001116134',
+          assignmentNo: 'CSE101-A2',
+          marks: 50,
+          details:
+            'Write a comprehensive essay on the principles of Object-Oriented Programming. Include examples and practical applications.',
+          uploadFileResources: [
+            {
+              diskType: 'application/pdf',
+              path: 'https://f004.backblazeb2.com/file/all-users/courseAssignments/6724fd912384ed2d3c261310-1732292999411-114278013-lorem-ipsum.pdf',
+              originalName: 'lorem-ipsum.pdf',
+              modifiedName:
+                '6724fd912384ed2d3c261310-1732292999411-114278013-lorem-ipsum.pdf',
+              fileId:
+                '4_z9b8c723f28a0f605912f081e_f10611b85118dd823_d20241122_m163002_c004_v0402027_t0029_u01732293002521',
+            },
+          ],
+        },
+      ],
+      tests: [
+        {
+          _id: '6740bcf4f5e9cfbcd868241b',
+          name: 'Math Final Exam',
+          type: 'MCQ',
+          time: 120,
+          publishDate: '2024-12-01T09:00:00.000Z',
+          questionList: [
+            {
+              questionId: '672e4044a2e8aa0a56602313',
+              _id: '6740bcf4f5e9cfbcd868241c',
+            },
+            {
+              newQuestion: {
+                type: 'MCQ',
+                title: 'What is 2 + 2?',
+                description:
+                  'Choose the correct answer from the given options.',
+                options: ['3', '4', '5', '6'],
+                correctOption: '4',
+              },
+              _id: '6740bcf4f5e9cfbcd868241d',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      _id: '6740a6d0477c8e8ffd6ee8d7',
+      number: 'Lesson 2',
+      name: 'HTML and CSS Fundamentals',
+      recodedClasses: [
+        {
+          _id: '6740ba003e34ecee75373550',
+          recodeClassName: 'What is CSS ?',
+          classDate: '2025-01-05T14:30:00.000Z',
+          classDetails: 'In-depth exploration of web devlopmen',
+          classVideoURL: ['https://youtube.com/watch?v=react-hooks-lesson1'],
+        },
+      ],
+      resources: [
+        {
+          _id: '6740af8e710f1c7ead9a0f96',
+          name: 'First Module Resources',
+          resourceDate: '2025-01-15T14:00:00.000Z',
+          uploadFileResources: [
+            {
+              diskType: 'application/pdf',
+              path: 'https://f004.backblazeb2.com/file/all-users/courseResources/6724fd912384ed2d3c261310-1732292489266-538491731-lorem-ipsum.pdf',
+              originalName: 'lorem-ipsum.pdf',
+              modifiedName:
+                '6724fd912384ed2d3c261310-1732292489266-538491731-lorem-ipsum.pdf',
+              fileId:
+                '4_z9b8c723f28a0f605912f081e_f103e83203219f7c4_d20241122_m162132_c004_v0402018_t0026_u01732292492855',
+            },
+          ],
+        },
+      ],
+      assignments: [],
+      tests: [],
+    },
+    {
+      _id: '6740a6d0477c8e8ffd6ee8d8',
+      number: 'Lesson 3',
+      name: 'JavaScript Basics',
+      recodedClasses: [],
+      resources: [],
+      assignments: [],
+      tests: [],
+    },
+    {
+      _id: '6740a6d0477c8e8ffd6ee8d9',
+      number: 'Lesson 4',
+      name: 'Responsive Web Design',
+      recodedClasses: [],
+      resources: [],
+      assignments: [],
+      tests: [],
+    },
+    {
+      _id: '6740a6d0477c8e8ffd6ee8da',
+      number: 'Lesson 5',
+      name: 'Advanced JavaScript Techniques',
+      recodedClasses: [],
+      resources: [],
+      assignments: [],
+      tests: [],
+    },
+    {
+      _id: '67472e4bf887dc58e94b5a87',
+      number: 'Lesson 6',
+      name: 'Master JavaScript',
+      recodedClasses: [],
+      resources: [],
+      assignments: [],
+      tests: [],
+    },
+    {
+      _id: '67472e4bf887dc58e94b5a88',
+      number: 'Lesson 7',
+      name: 'JavaScript Projects',
+      recodedClasses: [],
+      resources: [],
+      assignments: [],
+      tests: [],
+    },
+    {
+      _id: '67472f24f887dc58e94b5a8a',
+      number: 'Lesson 8',
+      name: 'Bootstrap',
+      recodedClasses: [],
+      resources: [],
+      assignments: [],
+      tests: [],
+    },
+    {
+      _id: '67472f24f887dc58e94b5a8b',
+      number: 'Lesson 9',
+      name: 'Tailwind',
+      recodedClasses: [],
+      resources: [],
+      assignments: [],
+      tests: [],
+    },
+  ],
+};
