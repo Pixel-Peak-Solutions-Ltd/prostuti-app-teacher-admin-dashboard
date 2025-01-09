192aae510751640805d663122700fdd2bb1c94b0
KaziMdMehediHasan <hassan.mehedee92@gmail.com>
2025-01-09 19:00:45 +0600

    eslint issues fixed

--- src/shared/components/SubjectFilter.tsx
+++ src/shared/components/SubjectFilter.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { CustomError } from '@/error';
 import useAxiosPrivate from '@/hooks/useAxiosPrivate';
 import useUserInfo from '@/hooks/useUserInfo';
@@ -7,12 +8,12 @@ import { useState } from 'react';
 import Select from 'react-select';
 
 const SubjectFilter = () => {
-    const [isOpen, setIsOpen] = useState(false)
-    const axiosPrivate = useAxiosPrivate()
+    const [isOpen, setIsOpen] = useState(false);
+    const axiosPrivate = useAxiosPrivate();
     const router = useRouter();
     const pathname = usePathname();
-    const searchParams = useSearchParams()
-    const params = new URLSearchParams(searchParams)
+    const searchParams = useSearchParams();
+    const params = new URLSearchParams(searchParams);
     const type = params.get('type') || '';
     const division = params.get('division') || '';
     const universityType = params.get('universityType') || '';
@@ -29,37 +30,37 @@ const SubjectFilter = () => {
                 {
                     withCredentials: true
                 }
-            )
-            return response.data
+            );
+            return response.data;
         }
-    })
+    });
 
     if (error)
         throw new CustomError(
             404,
             'Fetch Failed',
             'Failed to fetch the requested resource'
-        )
+        );
 
     const subjects = data?.data;
     const options = subjects?.map((subject: any) => ({
         value: subject,
         label: subject
-    }))
+    }));
     // const defaultOption = options?.find(
     //     (option: any) => option.value === territoryId
     // )
 
     const handleChange = (option: any) => {
-        const params = new URLSearchParams(searchParams)
+        const params = new URLSearchParams(searchParams);
         if (!option) {
-            params.delete('subject')
+            params.delete('subject');
         } else {
-            params.set('subject', option.value)
+            params.set('subject', option.value);
             // params.set('page', '1')
         }
-        router.replace(${ pathname } ? ${ params.toString() })
-    }
+        router.replace(pathname, params.toString());
+    };
     const customStyles = {
         control: (provided: any) => ({
             ...provided,
@@ -101,7 +102,7 @@ const SubjectFilter = () => {
             overflowY: 'auto',
             paddingRight: '2px'
         })
-    }
+    };
     return (
         <div className='flex flex-col gap-1 w-full sm:w-[10rem]'>
             <Select
@@ -114,7 +115,7 @@ const SubjectFilter = () => {
                 instanceId='subject'
             />
         </div>
-    )
-}
+    );
+};
 
 export default SubjectFilter;
