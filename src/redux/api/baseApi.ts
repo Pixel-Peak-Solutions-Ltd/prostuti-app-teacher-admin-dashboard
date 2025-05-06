f6ef4e6f0fc3074a18dba2f50fcc0a4c8266b129
G-Mursalin <golammursalin309@gmail.com>
2025-05-06 22:35:44 +0600

    Base API add

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -12,8 +12,7 @@ import { logout, setUser } from "../features/auth/authSlice";
 
 const baseUrl = import.meta.env.VITE_BASE_URL;
 const baseQuery = fetchBaseQuery({
-  // baseUrl:  `${baseUrl}/api/v1`,
-  baseUrl: `http://localhost:5000/api/v1`,
+  baseUrl: `${baseUrl}/api/v1`,
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
